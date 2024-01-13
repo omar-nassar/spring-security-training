4a1cf5bd3508279ebfe70d47b2370c4ee5d8f9b7
Omar Nassar <omar.nassar>
2024-01-13 11:00:40 +0300

    initial commit

---
+++ src/main/java/io/getarrays/api/init/ApplicationStartRunner.java
@@ -0,0 +1,59 @@
+package io.getarrays.api.init;
+
+import io.getarrays.api.model.Account;
+import io.getarrays.api.model.Role;
+import io.getarrays.api.repository.AccountRepository;
+import io.getarrays.api.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+
+@Component
+@RequiredArgsConstructor
+public class ApplicationStartRunner implements CommandLineRunner {
+    private final RoleRepository roleRepository;
+    private final AccountRepository accountRepository;
+
+    @Override
+    public void run(String ...args) throws Exception {
+//        Role roleUser = new Role(1L, "123", "ROLE_USER");
+//        Role roleAdmin = new Role(2L, "456", "ROLE_ADMIN");
+//        roleUser = roleRepository.save(roleUser);
+//        roleAdmin = roleRepository.save(roleAdmin);
+//
+//        Account superUser = new Account();
+//        superUser.setCredentialsexpired(false);
+//        superUser.setLocked(false);
+//        superUser.setPassword(new BCryptPasswordEncoder().encode("123"));
+//        superUser.setUsername("super_user");
+//        superUser.setRoles(Set.of(roleAdmin, roleUser));
+//
+//        accountRepository.save(superUser);
+//
+//        Account admin = new Account();
+//        admin.setCredentialsexpired(false);
+//        admin.setLocked(false);
+//        admin.setPassword(new BCryptPasswordEncoder().encode("123"));
+//        admin.setUsername("admin");
+//        admin.setRoles(Set.of(roleAdmin));
+//
+//        accountRepository.save(admin);
+//
+//
+//        Account user = new Account();
+//        user.setCredentialsexpired(false);
+//        user.setLocked(false);
+//        user.setPassword(new BCryptPasswordEncoder().encode("123"));
+//        user.setUsername("user");
+//        user.setRoles(Set.of(roleUser));
+//
+//        accountRepository.save(user);
+
+    }
+}
---
+++ src/main/java/io/getarrays/api/model/Account.java
@@ -0,0 +1,44 @@
+package io.getarrays.api.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.util.Set;
+
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.WRITE_ONLY;
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.CascadeType.MERGE;
+import static jakarta.persistence.FetchType.EAGER;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    @NotNull
+    private String username;
+    @JsonProperty(access = WRITE_ONLY)
+    @NotNull
+    private String password;
+    private boolean enabled = true;
+    private boolean credentialsexpired = false;
+    private boolean expired = false;
+    private boolean locked = false;
+    @ManyToMany(fetch = EAGER, cascade = MERGE)
+    @JoinTable(
+            name = "AccountRole",
+            joinColumns = @JoinColumn(name = "accountId", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "roleId", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
+}
